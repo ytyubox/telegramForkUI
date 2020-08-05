d239e101c98a7a16b3d5301e064fda47d70f7c0e
Tsungyu Yu <ytyubox@gmail.com>
2020-08-05 10:43:20 +0800

    Initial Commit

---
+++ Shared/telegramForkUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  telegramForkUIApp.swift
+//  Shared
+//
+//  Created by 游宗諭 on 2020/8/5.
+//
+
+import SwiftUI
+
+@main
+struct telegramForkUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
